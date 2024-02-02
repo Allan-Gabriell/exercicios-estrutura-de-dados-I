0237c75b9c17fcd42188b0f0c3e11bcab229600a
Allan Freitas <allangabrieldev@gmail.com>
2024-02-02 10:25:24 -0300

    Adicionei meus comentários

--- Unidade- II/lista-1/questao-1.c
+++ Unidade- II/lista-1/questao-1.c
@@ -9,31 +9,33 @@ de saída “saída_q3.txt”, como ilustrado a seguir.*/
 #include <string.h>
 
 int main(void){
+    //Crio minhas variáveis e meus vetores
     float notaA, notaB, notaC, media=0;
     char nome[100], linha[100];
 
-    FILE * arquivo_entrada = fopen("entrada_q3.txt", "r");
-    FILE * arquivo_saida = fopen("saida_q3.txt", "w");
+    FILE * arquivo_entrada = fopen("entrada_q3.txt", "r"); //crio meu arquivo de entrada
+    FILE * arquivo_saida = fopen("saida_q3.txt", "w"); //crio meu arquivo de saida
 
-    if(arquivo_entrada == NULL){
+    if(arquivo_entrada == NULL){ //verifico se foi criado de forma correta
         printf("Erro ao abrir/inicializar o arquivo de entrada!\n");
         return 1;
     }
 
-    if(arquivo_saida==NULL){
+    if(arquivo_saida==NULL){ //verifico se foi criado de forma correta também o meu arquivo de saida
         printf("Erro ao abrir/inicializar o arquivo de saída!\n"); 
         return 1; 
     }
 
-    while(fgets(linha,100,arquivo_entrada) != NULL){
-        if(sscanf(linha, "%s\t%f\t%f\t%f", nome, &notaA, &notaB, &notaC) != NULL){
-            media = (notaA + notaB + notaC)/3;
-            fprintf(arquivo_saida, "%s\t%.1f\t %s\n", nome, media, (media>=7.0)?"aprovado":"reprovado");
-        } else {
+    while(fgets(linha,100,arquivo_entrada) != NULL){ //crio um while que ler meu arquivo de entrada enquanto for diferente de nulo
+        if(sscanf(linha, "%s\t%f\t%f\t%f", nome, &notaA, &notaB, &notaC) != NULL){ //faço um if que separa meus dados lidos no arquivo de entrada por \t
+            media = (notaA + notaB + notaC)/3; //realizo o cálculo da minha média
+            fprintf(arquivo_saida, "%s\t%.1f\t %s\n", nome, media, (media>=7.0)?"aprovado":"reprovado"); //escrevo no meu arquivo de saida os dados que desejo
+        } else { //caso a condição do meu ir seja falsa
             printf("Erro na leitura da linha: %s\n", linha);
         }
     }
 
+    //encerro meus arquivos
     fclose(arquivo_entrada);
     fclose(arquivo_saida);
 
--- Unidade- II/lista-1/questao.2.c
+++ Unidade- II/lista-1/questao.2.c
@@ -7,31 +7,31 @@ como a média dos elementos do vetor, como ilustrado a seguir. */
 
 int main(void){
     float media;
-    int cont = 0, Min_num = 0, Max_num = 0, soma = 0, vect[10], num;
-    FILE *Entrada = fopen("arquivo_de_entrada.txt", "rt");
-    FILE *Saida = fopen("arquivo_de_saida.txt", "wt");
+    int cont = 0, Min_num = 0, Max_num = 0, soma = 0, vect[10], num; //declarei mminhas variáveis e o meu vetor de 10 posições
+    FILE *Entrada = fopen("arquivo_de_entrada.txt", "rt"); //Crio meu arquivo de leitura 
+    FILE *Saida = fopen("arquivo_de_saida.txt", "wt"); //Crio meu arquivo de escrita
 
-    if(Entrada == NULL){
+    if(Entrada == NULL){ //Verifico se ocorreu tudo certo na criação do meu arquivo de entrada
         printf("Erro ao abrir o arquivo de entrada!\n");
         exit(1);
     }
 
-    if(Saida == NULL){
+    if(Saida == NULL){ //Verifico se ocorreu tudo certo na criação do meu arquivo de saida
         printf("Erro ao abrir o arquivo de saída!\n");
         exit(1);
     }
 
-    for(int i =0; i<10; i++){
-        if(fscanf(Entrada, "%d\n", &num) == NULL){
+    for(int i =0; i<10; i++){ //Crio um loop de 10 repetições
+        if(fscanf(Entrada, "%d\n", &num) == NULL){ //Verifico se eu tenho algo para ler no meu arquivo de entrada
             printf("Erro na leitura do arquivo de entrada! \n");
             return 1;
         }
-        vect[i] = num;
-        soma += num;
-        if( i == 0){
+        vect[i] = num; //Pego o valor de num e atribuo a vect na posição i
+        soma += num; //Realizo a somatória dos valores
+        if( i == 0){ //Crio um if para atribuir valores iniciais a i para Max_num e Min_num
             Min_num = num;
             Max_num = num;
-        } else{
+        } else{ //Nesse else eu faço eu realizo a busca pelo maior e o menor valor do arquivo de entrada
             if(num < Min_num){
                 Min_num = num;
             }
@@ -41,8 +41,9 @@ int main(void){
         }
     }
 
-    media = (float) soma / 10;
+    media = (float) soma / 10; // realizo o cálculo da média e forço para que seja um retorno do tipo float e armazeno em media
 
+    //Escrevo no meu arquivo de saída os dados que desejo
     fprintf(Saida, "Menor número: %d\n", Min_num);
     fprintf(Saida, "Maior número: %d\n", Max_num);
     fprintf(Saida, "Média: %.2f\n", media);
